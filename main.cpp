0cd8cb6138e56bc43c09e6a1ff8a0eb80cc83644
YongJer Chuang <jerjer920813@gmail.com>
2022-07-10 23:23:36 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::sort;
+int main(){
+    return 0;
+}
